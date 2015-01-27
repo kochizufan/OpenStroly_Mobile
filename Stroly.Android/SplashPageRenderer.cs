bd9545bd6fc2f7690d78a27f2dc8655b407c197a
OHTSUKA Ko-hei <otsuka.kohei@hpsys.co.jp>
2015-01-27 16:41:15 +0900

    PageRenderer作成

---
+++ Stroly.Android/SplashPageRenderer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Stroly;
+using Stroly.Android;
+
+[assembly: ExportRenderer (typeof (SplashPage), typeof (SplashPageRenderer))]
+
+namespace Stroly.Android
+{
+	public class SplashPageRenderer : PageRenderer
+	{
+		protected override void OnAttachedToWindow ()
+		{
+			base.OnAttachedToWindow ();
+		}
+
+		protected override void OnDetachedFromWindow ()
+		{
+			base.OnDetachedFromWindow ();
+		}
+	}
+}
+
+
---
+++ Stroly.iOS/SplashPageRenderer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using Stroly;
+using Stroly.iOS;
+
+[assembly: ExportRenderer (typeof (SplashPage), typeof (SplashPageRenderer))]
+
+namespace Stroly.iOS
+{
+	public class SplashPageRenderer : PageRenderer
+	{
+		public override void ViewWillAppear (bool animated)
+		{
+			base.ViewWillAppear (animated);
+		}
+
+		public override void ViewDidDisappear (bool animated)
+		{
+			base.ViewDidDisappear (animated);
+		}
+	}
+}
+
