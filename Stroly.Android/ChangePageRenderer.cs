59659f1214ef4680bc12615c76cbc1d298935e31
OHTSUKA Ko-hei <otsuka.kohei@hpsys.co.jp>
2015-01-28 13:16:47 +0900

    一時退避

---
+++ Stroly.Android/ChangePageRenderer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Stroly.Android
+{
+	public class ChangePageRenderer : ContentPage
+	{
+		public ChangePageRenderer ()
+		{
+			Content = new StackLayout { 
+				Children = {
+					new Label { Text = "Hello ContentPage" }
+				}
+			};
+		}
+	}
+}
+
+
